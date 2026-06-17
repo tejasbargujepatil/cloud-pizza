7980d76e942f7b00e777efa51b64de793900bac8
Tejas Barguje Patil <your-email@example.com>
2026-06-17 11:36:09 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>🍕 Welcome to Cloud Pizza Factory</h1>
+    <h2>Today's Special: Paneer Tikka Pizza</h2>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
